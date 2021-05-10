08d6140d93c58f355d9abdea3909cd5b5cc95296
sak96 <>
2021-05-10 21:12:20 +0530

    Use local data dir for creating database

--- src/db/mod.rs
+++ src/db/mod.rs
@@ -3,18 +3,33 @@ pub mod schema;
 
 use diesel::{connection::Connection, SqliteConnection};
 
-#[cfg(not(test))]
-const DATABASE_URL: &str = "target/prod.db";
-
-#[cfg(test)]
-const DATABASE_URL: &str = ":memory:";
-
 embed_migrations!();
 
 pub type DbConnection = SqliteConnection;
 
 pub fn establish_connection() -> Result<SqliteConnection, String> {
-    let connection = SqliteConnection::establish(&DATABASE_URL).map_err(|e| e.to_string())?;
+    #[cfg(not(test))]
+    let db_url = {
+        use std::fs::create_dir_all;
+
+        // get local data dir
+        let mut path = dirs::data_local_dir().expect("should have local data dir");
+
+        // create local_data_dir/<app>/
+        path.push(structopt::clap::crate_name!());
+        if !path.exists() {
+            create_dir_all(&path).expect(&format!("creating dir {:#?} failed", &path));
+        }
+
+        // set path to local_data_dir/<app>/<app.db>
+        path.push(format!("{}.db", structopt::clap::crate_name!()));
+        path.to_string_lossy().into_owned()
+    };
+
+    #[cfg(test)]
+    let db_url = ":memory:";
+
+    let connection = SqliteConnection::establish(&db_url).map_err(|e| e.to_string())?;
     embedded_migrations::run(&connection).map_err(|e| e.to_string())?;
     Ok(connection)
 }
